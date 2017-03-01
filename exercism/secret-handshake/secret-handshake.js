5812bcbc9084097d7a3deefdc4d7a30561d821e9
Breyana Scales <breyana@Breyanas-MacBook-Pro.local>
2017-02-28 16:04:27 -0800

    Secret Handshake

--- exercism/secret-handshake/secret-handshake.js
+++ exercism/secret-handshake/secret-handshake.js
@@ -7,26 +7,48 @@ var SecretHandshake = function(input) {
 
 SecretHandshake.prototype.commands = function() {
   //convert this.number to binary
-  let number = this.number
-  let binaryArray = []
+let number = this.number
+let binaryArray = []
+let handshakeArray = []
 
-  while (number >= 1) {
-    converted.unshift(number%2)
-    number = Math.floor(number/2)
+while (number >= 1) {
+  binaryArray.unshift(number%2)
+  number = Math.floor(number/2)
+}
+
+let binaryNumber = parseInt(binaryArray.join(''))
+
+const shakes = {
+  "jump": 1000,
+  "close your eyes": 100,
+  "double blink": 10,
+  "wink": 1
+  // 10000: function() {
+  //   //reverse order of operations
+  // }
+}
+
+if (binaryNumber >= 10000) {
+  var pushHandshakes = function(input) {
+    handshakeArray.push(input)
+  }
+  binaryNumber -= 10000
+} else {
+  var pushHandshakes = function(input) {
+    handshakeArray.unshift(input)
   }
 
-  let binaryNumber = parseInt(binaryArray.join(''))
-  //
+}
 
-  const shakes = {
-    1: "wink"
-    10: "double blink"
-    100: "close your eyes"
-    1000: "jump"
-    10000: function() {
-      //reverse order of operations
-    }
+for (let key in shakes) {
+  if (binaryNumber >= shakes[key]) {
+    pushHandshakes(key)
+    binaryNumber -= shakes[key]
   }
+}
+
+return handshakeArray
+
 };
 
 module.exports = SecretHandshake;
